9766b9a4ca9ac2629509e7377c927c0ffc25360a
Thomas Daniel <daniel.2.tg@gmail.com>
2018-01-15 14:33:59 +1100

    App now meets MVC design protocol.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-
 //NOTE: Using a UITableViewController instead of View Controller with table means delegating etc. is all taken care of.
 class ToDoListViewController: UITableViewController {
-
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    //This is an array fo item objects.
+    var itemArray = [Item]()
     
     //Setup up user defaults constant.
     let defaults = UserDefaults.standard
@@ -21,12 +21,24 @@ class ToDoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
         //This loads the defaults persistent data into the itemArray.
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            
+    if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+
             itemArray = items
-            
+
         }
         
     }
@@ -46,8 +58,17 @@ override func tableView(_ tableView: UITableView, numberOfRowsInSection section:
     //This lets cell = the cells in UITableView.
     let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
     
+        let item = itemArray[indexPath.row]
+        
+        //NOTE: This ensures that the checkmark is attributed to the boolean value.
+        //NOTE: This is applying the ternary operator.
+        //Essentially shrunken if else.
+        //Format: value = condition ? valueIfTrue : ValueIfFalse
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+    
     //This allocations the cell value based on row value looking up into array.
-    cell.textLabel?.text = itemArray[indexPath.row]
+    cell.textLabel?.text = item.title
     
     //This sends cell vale to table view.
     return cell
@@ -58,17 +79,13 @@ override func tableView(_ tableView: UITableView, numberOfRowsInSection section:
     //This allows the cells to have a checkmark.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-        
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        } else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //Essentially this replaces wordy if statement to make it change to opposite value.
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         //NOTE: This makes sure the cell does not stay selected.
         tableView.deselectRow(at: indexPath, animated: true)
+        
+         tableView.reloadData()
     }
         
     //MARK: Add New Items
@@ -88,8 +105,10 @@ override func tableView(_ tableView: UITableView, numberOfRowsInSection section:
             if textField.text == "" {
             
             } else {
-                
-            self.itemArray.append(textField.text!)
+            
+                let newItem = Item()
+                newItem.title = textField.text!
+                self.itemArray.append(newItem)
                 
             //Update user defaults. Can add any datat type and then associate a key to return it.
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tom Daniel Home on 14/1/18.
+//  Copyright © 2018 Thomas Daniel. All rights reserved.
+//
+
+import Foundation
+
+//Note you do not have to name your class the same BUT 99% of the time you do.
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
